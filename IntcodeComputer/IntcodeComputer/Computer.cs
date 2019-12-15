3f976f7cad9ed612ac2c4b3e69feeed6229fc0a4
Esli Stavenga <eslistavenga@hotmail.com>
2019-12-15 21:49:20 +0100

    Added IntComputer
    Got rid off the Day2 project, it's irrelevant now :)
    Added IntComputer project, it's looking pretty slick

---
+++ IntcodeComputer/IntcodeComputer/Computer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using IntcodeComputer.Opcodes;
+
+namespace IntcodeComputer
+{
+    public class Computer
+    {
+
+        public string Commands { get; set; }
+        private List<Opcode> opcodes = new List<Opcode>();
+
+        public Computer()
+        {
+            opcodes.Add(new AdditionOpcode());
+            opcodes.Add(new TerminateOpcode());
+            opcodes.Add(new MultiplicationOpcode());
+        }
+
+        public int[] Run()
+        {
+            int[] input = this.Commands.Split(',').Select(x => int.Parse(x)).ToArray();
+            int index = 0;
+            int currentCode = input[index];
+            int terminateOpcode = opcodes.Where(x => x.GetType() == typeof(TerminateOpcode)).Single().IntCode;
+
+            do
+            {
+
+                foreach (Opcode opcode in opcodes)
+                {
+                    if (currentCode % 100 == opcode.IntCode)
+                    {
+                        input = opcode.Execute(input, index);
+                        index += opcode.InputLength;
+                        break;
+                    }
+                }
+
+            } while ((currentCode = input[index]) != terminateOpcode);
+
+            return input;
+
+        }
+
+    }
+}
---
+++ IntcodeComputer/IntcodeComputer/Opcodes/Opcode.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace IntcodeComputer.Opcodes
+{
+    internal abstract class Opcode : IOpcode
+    {
+
+        public int IntCode { get; protected set; } = 99;
+        public int InputLength { get; protected set; } = 0;
+
+        protected enum PARAMETER_MODES { POSITION = 0, IMIDIATE = 1 }
+
+        protected int getVariableFromPositionMode(int[] input, int index)
+        {
+            return getVariableFromImidiateMode(input, input[index]);
+        }
+
+        protected int getVariableFromImidiateMode(int[] input, int index)
+        {
+            return input[index];
+        }
+
+        protected int[] setVariableFromPositionMode(int[] input, int index, int newValue)
+        {
+            return setVariableFromImidiateMode(input, input[index], newValue);
+        }
+
+        protected int[] setVariableFromImidiateMode(int[] input, int index, int newValue)
+        {
+            input[index] = newValue;
+
+            return input;
+        }
+
+        protected int getVariable(int[] input, int index, PARAMETER_MODES mode = PARAMETER_MODES.POSITION)
+        {
+            if(mode == PARAMETER_MODES.POSITION)
+            {
+                return this.getVariableFromPositionMode(input, index);
+            } else
+            {
+                return this.getVariableFromImidiateMode(input, index);
+            }
+        }
+
+        protected int[] setVariable(int[] input, int index, int newValue, PARAMETER_MODES mode = PARAMETER_MODES.POSITION)
+        {
+            if(mode == PARAMETER_MODES.POSITION)
+            {
+                return this.setVariableFromPositionMode(input, index, newValue);
+            } else
+            {
+                return this.setVariableFromImidiateMode(input, index, newValue);
+            }
+        }        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="opcode"></param>
+        /// <param name="parameter"> The xth parameterm, 1, 2, 3 etc</param>
+        /// <returns></returns>
+        protected PARAMETER_MODES getModeByParameterPos(int opcode, int parameter)
+        {
+            string code = opcode.ToString("D5");
+//            int opcodeLength = code.Length;
+            PARAMETER_MODES returnValue = PARAMETER_MODES.POSITION; //default mode is position mode
+            
+            //calculate the index needed
+            //e.g. parameter required = 3, 3 - 3 = 0
+            //parameter required = 1, 3 - 1 = 2
+            int index = 3 - parameter; 
+            if(code[index] == '1') //set mode to imidiate
+            {
+                returnValue = PARAMETER_MODES.IMIDIATE;
+            }
+
+
+            return returnValue;
+        }
+
+        public virtual int[] Execute(int[] input, int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+//1002
+//10002
+//00102
+
